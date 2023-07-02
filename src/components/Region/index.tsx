3f065b30a87a37c6ba34bf5e8eb8c8738a13bb9f
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-07-01 23:36:20 -0300

    feat: Region with champions' list

--- src/components/Region/index.tsx
+++ src/components/Region/index.tsx
@@ -1,3 +1,5 @@
+import noPhotoChampion from '../../assets/images/noPhotoChampion.png'
+
 export const Region = () => {
     const urlCurrent = document.URL
     const questionMarkIndex = urlCurrent.indexOf("?")
@@ -19,13 +21,43 @@ export const Region = () => {
         "zaun": ["Blitzcrank", "Dr. Mundo", "Ekko", "Janna", "Jinx", "Renata Glasc", "Singed", "Twitch", "Urgot", "Viktor", "Warwick", "Zac", "Ziggs", "Zeri"]
     }
 
-    const champList = champByRegion[`${regionChosen}`]
+    const champList : Array<string> = champByRegion[regionChosen]
+
+    const fixNameInURL = (champName: string) => {
+        let champNameUpdated: string = champName
+        switch (champNameUpdated) {
+            case "Wukong":
+                champNameUpdated = "MonkeyKing"
+                break
+            case "LeBlanc":
+            case "Bel'Veth":
+            case "Cho'Gath":
+            case "Kai'Sa":
+            case "Kha'Zix":
+                champNameUpdated = (champNameUpdated.charAt(0) + champNameUpdated.slice(1).toLowerCase()).replace("'", "")
+                break
+            case "Nunu & Willump":
+            case "Renata Glasc":
+                champNameUpdated = champNameUpdated.split(" ", 1)[0]
+                break
+            default:
+                champNameUpdated = champNameUpdated.replace(" ", "").replace("'", "").replace(".", "")
+                break
+        }
+        return champNameUpdated
+    }
 
     return (
-        <div className='flex justify-around mt-10 mb-20 ms-10'>
-            {regionChosen}
-            <ul>
-                {champList.map(champ => <li>{champ}</li>)}
+        <div className="mb-20 mx-20">
+            {/* {regionChosen} */}
+            <ul className='flex flex-wrap justify-around gap-7'>
+                {champList.map((champName: string, key: number) => <li className="w-1/6" key={key}>
+                    <img id="primaryImage" src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${fixNameInURL(champName)}_0.jpg`} alt={champName} onError={({ currentTarget }) => {
+                        currentTarget.onerror = null; // prevents looping (font: https://stackoverflow.com/questions/34097560/react-js-replace-img-src-onerror)
+                        currentTarget.src = noPhotoChampion;
+                    }} />
+                    <p>{champName}</p>
+                </li>)}
             </ul>
         </div>
     )
