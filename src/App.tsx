ad179e6f27937b4580e48c5c3643150c5588ae49
gabrielsoarespebr <gabrielsoares.pe.br@gmail.com>
2023-07-03 11:34:37 -0300

    feat: Footer (hoover in web version, not in mobile)

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,17 @@ function App() {
         <Route path='/region' element={<Region />} />
       </Routes>
 
+      <a href="https://github.com/gabrielsoarespebr" target='_blank'>
+        <footer className='flex justify-center gap-2 my-20 drop-shadow-md md:opacity-40 hover:opacity-100 duration-700'>
+          <img className='w-20 rounded-full' src="https://static.wikia.nocookie.net/leagueoflegends/images/1/16/Doom_Icon_of_Doom_profileicon.png" alt="Ziggs Icon" />
+          <div className='flex flex-col justify-center items-center text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-400'>
+            <p>Developed by <span className='text-fuchsia-400'>Gabriel Soares</span></p>
+            <p className='text-xs'>How about to play ARAM together?</p>
+            <p className='text-xs'>Invite me: <span className='text-fuchsia-400'>Russo Russiere</span></p>
+          </div>
+        </footer>
+      </a>
+
       <div className='fixed bottom-0'>
         <PoroScroll />
       </div>
